b7239321b86306c9a18eb44b653512fe8181c6da
Jonathan Thom <jonathan.thom1990@gmail.com>
2017-03-16 12:36:09 -0700

    User page work in progress

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,8 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :trackable, :validatable
   has_many :exercises
   validates :first_name, :last_name, :presence => true
+
+  def name
+    self.first_name + " " + self.last_name
+  end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,4 +4,11 @@ RSpec.describe User, type: :model do
   it { should validate_presence_of :first_name }
   it { should validate_presence_of :last_name }
   it { should have_many :exercises }
+
+  describe '#name' do
+    it 'will return the user\'s full name' do
+      user = User.create(first_name: 'Foo', last_name: 'Bar', email: 'foo@bar.com', password: 'password', password_confirmation: 'password')
+      expect(user.name).to eq('Foo Bar')
+    end
+  end
 end
